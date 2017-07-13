247c77a81276bc5defd2af4316e4bbbef821f037
drebap <drebapt@gmail.com>
2017-07-13 13:35:17 +0100

    Make Queen structure public

--- nqueen/nqueen.go
+++ nqueen/nqueen.go
@@ -7,14 +7,14 @@ import (
 // N-Queen structure is a board of integers where the index represents
 // the column of the queen in the board and the content represents the
 // line of the queen.
-type queen struct {
+type Queen struct {
 	board []int
 }
 
 // Make a new N-Queen object with a board of mixed values after
 // a sequential initialization.
-func Make(size int) queen {
-	q := queen{board: make([]int, size)}
+func Make(size int) Queen {
+	q := Queen{board: make([]int, size)}
 
 	for i := 0; i < size; i++ {
 		q.board[i] = i
@@ -27,24 +27,24 @@ func Make(size int) queen {
 
 // duplicates replicates a N-Queen object by creating a new one
 // and copying its contents to the new one.
-func (q *queen) duplicate() queen {
+func (q *Queen) duplicate() Queen {
 	newBoard := make([]int, len(q.board))
 
 	for i := 0; i < len(q.board); i++ {
 		newBoard[i] = q.board[i]
 	}
 
-	newQueen := queen{board: newBoard}
+	newQueen := Queen{board: newBoard}
 	return newQueen
 }
 
 // randInt generates a random integer from 0 to the size of the board
-func (q queen) randInt() int {
+func (q Queen) randInt() int {
 	return rand.Intn(len(q.board))
 }
 
 // swapTwo swaps two random queens from the N-Queens board
-func (q *queen) swapTwo() {
+func (q *Queen) swapTwo() {
 	first := q.randInt()
 	second := q.randInt()
 
@@ -53,21 +53,21 @@ func (q *queen) swapTwo() {
 
 // mixBoard mixes the N-Queens board by swapping two random queens as many times
 // as the size of the board.
-func (q *queen) mixBoard() {
+func (q *Queen) mixBoard() {
 	for i := 0; i < len(q.board); i++ {
 		q.swapTwo()
 	}
 }
 
 // areThreats checkes if two given queens threat each other.
-func (q *queen) areThreats(first int, second int) bool {
+func (q *Queen) areThreats(first int, second int) bool {
 	return q.board[first]-first == q.board[second]-second ||
 		q.board[first]+first == q.board[second]+second ||
 		q.board[first] == q.board[second]
 }
 
 // Heuristic function returns the number of threats in a board of N-Queens.
-func (q *queen) Heuristic() int {
+func (q *Queen) Heuristic() int {
 	threats := 0
 
 	for i := 0; i < len(q.board); i++ {
@@ -82,16 +82,16 @@ func (q *queen) Heuristic() int {
 
 // Objective function checks if a given board is a solution to the problem,
 // that is, if its heuristic is 0.
-func (q *queen) Objective() bool {
+func (q *Queen) Objective() bool {
 	return q.Heuristic() == 0
 }
 
 // Sucessor generates a possible list of successors and selects the first
 // one found where its heuristic is smaller or equal than the current one.
-func (q *queen) Successor() *queen {
+func (q *Queen) Successor() *Queen {
 	var (
 		listSize         = len(q.board) * 5
-		successors       = make([]queen, listSize)
+		successors       = make([]Queen, listSize)
 		currentHeuristic = q.Heuristic()
 	)
 
@@ -112,7 +112,7 @@ func (q *queen) Successor() *queen {
 
 // Solve function is the function that solves the problem. This is an
 // implementation of the hill-climbing algorithm with restarts.
-func (q *queen) Solve() queen {
+func (q *Queen) Solve() Queen {
 	current := Make(len(q.board))
 
 	for {
