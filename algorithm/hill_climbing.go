bd36619462a63bb192d6d001cdaaa1c8d21db687
drebap <drebapt@gmail.com>
2017-07-14 01:01:29 +0100

    Generic Hill-Climbing algorithm added.
    Created a generic Hill-Cimbing algorithm for any type that
    implements interface HillClimbingSolver.

---
+++ algorithm/hill_climbing.go
@@ -0,0 +1,28 @@
+package algorithm
+
+type HillClimbingSolver interface {
+	New(size int) HillClimbingSolver
+	Successor() HillClimbingSolver
+	Objective() bool
+}
+
+func Solve(size int, h HillClimbingSolver) *HillClimbingSolver {
+	current := h.New(size)
+
+	for {
+		for i := 0; i < size*3; i++ {
+			successor := current.Successor()
+
+			if successor != nil {
+				current = successor
+				break
+			} else {
+				continue
+			}
+		}
+
+		if current.Objective() {
+			return &current
+		}
+	}
+}
