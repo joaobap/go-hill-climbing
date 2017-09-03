6650223afbfc733e7335247543e16a6e7c2764d2
drebap <drebapt@gmail.com>
2017-09-03 02:17:31 +0100

    Removed unecessary packages.
    Moved HillClimbing interface to nqueen file and corrected a error
    with goroutines that were not working

--- main.go
+++ main.go
@@ -6,19 +6,16 @@ import (
 	"os"
 	"strconv"
 	"time"
-
-	"github.com/errbap/goia/algorithm"
-	"github.com/errbap/goia/nqueen"
 )
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	size, _ := strconv.Atoi(os.Args[1])
 
-	q := nqueen.Make(size)
+	q := MakeQueen(size)
 
-	resInterface := algorithm.HillClimbing(size, q)
-	result := nqueen.Queen(resInterface.(nqueen.Queen))
+	resInterface := SolveWithHillClimbing(size, q)
+	result := Queen(resInterface.(Queen))
 
 	fmt.Printf("Final state:\n%v\n", result)
 	fmt.Printf("Heuristic:\n%v\n", result.Heuristic())
--- nqueen.go
+++ nqueen.go
@@ -1,10 +1,47 @@
-package nqueen
+package main
 
 import (
 	"math/rand"
-
-	"github.com/errbap/goia/algorithm"
+	"reflect"
 )
+
+// HillClimbingSolver is an interface that must be met to
+// solve a problem using the Hill-Climbing algorithm.
+// Sucessor is responsable for the method used to generate the
+// successor, first choice or best choice.
+type HillClimbingSolver interface {
+	// New creates a new object of the problem to be solved.
+	New(size int) HillClimbingSolver
+
+	// Successor generates a new successor state from a problem object.
+	Successor() HillClimbingSolver
+
+	// Objective checks if the current state is an objective state.
+	Objective() bool
+}
+
+// SolveWithHillClimbing is an implementation of the Hill Climbing algorithm
+// for local search problems.
+func SolveWithHillClimbing(size int, h HillClimbingSolver) HillClimbingSolver {
+	current := h.New(size)
+
+	for {
+		for i := 0; i < size*3; i++ {
+			successor := current.Successor()
+
+			// Found better successor
+			if !reflect.DeepEqual(successor, current) {
+				current = successor
+			} else {
+				break
+			}
+		}
+
+		if current.Objective() {
+			return current
+		}
+	}
+}
 
 // N-Queen structure is a board of integers where the index represents
 // the column of the queen in the board and the content represents the
@@ -13,9 +50,9 @@ type Queen struct {
 	board []int
 }
 
-// Make a new N-Queen object with a board of mixed values after
-// a sequential initialization.
-func Make(size int) Queen {
+// MakeQueen creates a new N-Queen object with a board of mixed values
+// after a sequential initialization.
+func MakeQueen(size int) Queen {
 	qq := Queen{board: make([]int, size)}
 
 	for i := 0; i < size; i++ {
@@ -29,8 +66,8 @@ func Make(size int) Queen {
 
 // New implements the HillClimbingSolver interface function to
 // create a new N Queen object.
-func (q Queen) New(size int) algorithm.HillClimbingSolver {
-	return Make(size)
+func (q Queen) New(size int) HillClimbingSolver {
+	return MakeQueen(size)
 }
 
 // BoardSize returns the size of the current board.
@@ -79,24 +116,16 @@ func (q *Queen) areThreats(first int, second int) bool {
 		q.board[first] == q.board[second]
 }
 
-func HeuristicChan(c chan int) {
-	threats := 0
-
-	for j := i + 1; j < len(q.board); j++ {
-		if q.areThreats(i, j) {
-			threats++
-		}
-	}
-
-	c <- threats
-}
-
 // Heuristic function returns the number of threats in a board of N-Queens.
 func (q *Queen) Heuristic() int {
-	c := make(chan int)
+	threats := 0
 
 	for i := 0; i < len(q.board); i++ {
-		go HeuristicChan(c)
+		for j := i + 1; j < len(q.board); j++ {
+			if q.areThreats(i, j) {
+				threats++
+			}
+		}
 	}
 	return threats
 }
@@ -126,6 +155,6 @@ func (q Queen) successor() Queen {
 
 // Successor implements the HillClimbingSolver interface function in
 // order to generate a new successor from a internal board.
-func (q Queen) Successor() algorithm.HillClimbingSolver {
+func (q Queen) Successor() HillClimbingSolver {
 	return q.successor()
 }
